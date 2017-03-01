c75ca43c47894e38ebc83f6948a5fd34b8750dac
Ilia Kebets <kebetsi@gmail.com>
2017-03-01 08:31:44 +0100

    implement multiple-heavy requests and single-heavy-multiple-light

--- benchmark/multiple-heavy.sh
+++ benchmark/multiple-heavy.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
 echo TODO
+
+echo "########################"
+echo "######### 10k ##########"
+echo "########################"
+ab -n 10 -c 10 -l "http://127.0.0.1:3000/benchmark/events/?auth=cizk09ayw0004p7ot4xv0sl3n&limit=10000"
+
+echo "########################"
+echo "######### 100k #########"
+echo "########################"
+ab -n 10 -c 10 -l "http://127.0.0.1:3000/benchmark/events/?auth=cizk09ayw0004p7ot4xv0sl3n&limit=100000"
--- benchmark/single-heavy-multiple-light.sh
+++ benchmark/single-heavy-multiple-light.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo TODO
+echo "########################"
+echo "##### heavy: 500k ######"
+echo "########################"
+time curl -s "http://127.0.0.1:3000/benchmark/events/?auth=cizk09ayw0004p7ot4xv0sl3n&limit=500000" > /dev/null &
+
+echo "########################"
+echo "#### 20 light : 20 #####"
+echo "########################"
+ab -n 20 -c 10 -l "http://127.0.0.1:3000/benchmark/events/?auth=cizk09ayw0004p7ot4xv0sl3n&limit=20"
