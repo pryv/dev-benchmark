d567a50258d4aaac3a56edb2364773e4af4591a9
Ilia Kebets <kebetsi@gmail.com>
2017-05-17 21:32:52 +0200

    update users generation

--- src/data-generation/users.js
+++ src/data-generation/users.js
@@ -1,24 +1,49 @@
 const request = require('superagent');
 const async = require('async');
 const _ = require('lodash');
+const fs = require('fs');
+
+const OUTPUT_USERS_FILE = __dirname + '/../../json-data/users.json';
+
+let argv = require('minimist')(process.argv.slice(2));
+
+let domain = argv.domain;
+let hosting = argv.hosting;
+let prefix = argv.prefix;
+let numUsers = argv.num;
+
+console.log(argv);
+
+if (! domain) {
+  domain = 'pryv.li';
+}
+if (! hosting) {
+  hosting = 'exoscale.ch-ch2';
+}
+if (! prefix) {
+  prefix = 'testuser-';
+}
+if (! numUsers) {
+  numUsers = 100;
+}
 
 let userCreateBase = {
-  "hosting": "exoscale.ch-ch2",
-  "appid": "dev-benchmark",
-  "username": "",
-  "email": "development-tests-001@bogus.tk",
-  "password": "testdeuxfois",
-  "invitationtoken": "enjoy"
+  hosting: hosting,
+  appid: 'dev-benchmark',
+  username: '',
+  email: '',
+  password: '',
+  invitationtoken: 'enjoy'
 };
 
 let usersToCreate = [];
 
-for (let i = 0; i < 100; i++) {
+for (let i = 0; i < numUsers; i++) {
   usersToCreate.push(
     _.extend(_.clone(userCreateBase), {
-      username: 'testuser2-' + i,
-      email: 'testuser2-' + i + '@benchmark.tk',
-      password: 'testuser2-' + i
+      username: prefix + i,
+      email: prefix + i + '@benchmark.tk',
+      password: prefix + i
     })
   )
 }
@@ -27,20 +52,21 @@ usersToCreate.forEach((u) => {
   usersStripped.push(_.pick(u, ['username', 'password']));
 });
 
+fs.writeFileSync(OUTPUT_USERS_FILE, JSON.stringify(usersStripped));
 
-const fs = require('fs');
-fs.writeFileSync('users.json', JSON.stringify(usersStripped));
+createUsers(usersToCreate);
 
-
-async.eachSeries(usersToCreate, (user, cb) => {
-  request.post('https://reg.pryv.li/user')
-    .send(user)
-    .set('Content-Type', 'application/json')
-    .end((err, res) => {
-      if (err) {
-        console.log('error, failed to create user:', user.username);
-      }
-      console.log('res', res.body)
-      cb();
-    });
-});
+function createUsers (users) {
+  async.eachSeries(usersToCreate, (user, cb) => {
+    request.post('https://reg.pryv.li/user')
+      .send(user)
+      .set('Content-Type', 'application/json')
+      .end((err, res) => {
+        if (err) {
+          console.log('error, failed to create user:', user.username);
+        }
+        console.log('res', res.body)
+        cb();
+      });
+  });
+}
