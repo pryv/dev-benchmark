52247c2e818e3cbd9fecfa58c975bf5687d16773
Perki <perki@pryv.com>
2021-11-03 18:17:29 +0100

    some refactor

--- src/launchers/hello-server.js
+++ src/launchers/hello-server.js
@@ -1,19 +1,26 @@
 var http = require('http');
-const { Readable } = require('stream');
+const { Readable, PassThrough } = require('stream');
+
+const amount = 10000;
 
 //create a server object:
 http.createServer(function (req, res) {
   console.log(req.url);
   switch (req.url) {
     case '/':
-      for (let i = 0; i < 10; i++) {
-        res.write('Hello World!'); //write a response to the client
+      let x = '';
+      for (let i = 0; i < amount; i++) {
+        x += 'Hello World!';
+        //res.write('Hello World!'); // equivalent to streaming when growing in size (amount)
       }
+      res.write(x);
       res.end(); //end the response
       break;
+
     case '/streamed':
       streamData(res);
       break;
+
     default:
     res.statusCode = 400;
     res.write('Unkown'); //write a response to the client
@@ -24,15 +31,12 @@ http.createServer(function (req, res) {
 console.log('Ready');
 
 function streamData(res) {
-  
   const readable = new Readable()
-
   readable.pipe(res)
 
-  for (let i = 0 ; i < 10; i++) {
+  for (let i = 0 ; i < amount; i++) {
     readable.push('Hello World!');
   }
   // no more data
-  readable.push(null)
-
+  readable.push(null);
 }
